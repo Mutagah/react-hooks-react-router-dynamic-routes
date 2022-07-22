6b57a0e18fa2419f78be05d82bc22a1fef7ec3db
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-07-22 12:36:27 +0300

    Implemented nesting of routes

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Route } from "react-router-dom";
+//useRouteMatch hook is used to identify a matched route from React Router
+import { Route,useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
-
+import MovieShow from "./MovieShow";
 function MoviesPage({ movies }) {
+//useRouteMatch returns a special object with information about the
+//currently matched route
+  const match = useRouteMatch();
+  console.log(match)
   return (
     <div>
       <MoviesList movies={movies} />
+      {/*Using the current Url from the `match` object
+      as a part of the path;
+      this will generate a url like "movies/:movieId" */}
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies}/>
+      </Route>
     </div>
   );
 }
 export default MoviesPage;
+
